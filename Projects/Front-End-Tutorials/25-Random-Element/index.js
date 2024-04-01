7ea6e070118cc5dee3aca66950299b132cd85c2a
Mos3aB696 <mos3ab696@gmail.com>
2024-04-01 23:55:32 +0200

    Delete Projects Files

--- Projects/Front-End-Tutorials/25-Random-Element/index.js
+++
@@ -1,7 +0,0 @@
-let arr = ["Mos3aB", "Ahmed", "Ali", "Ibrahim"];
-
-console.log(Math.trunc(Math.random() * arr.length));
-
-console.log(arr[Math.trunc(Math.random() * arr.length)]);
-document.querySelector(".winner").innerHTML =
-  arr[Math.trunc(Math.random() * arr.length)];
