49d6200e37e29431fdc878d3a6b35278b2a7132a
Mos3ab696 <mos3ab696@gmail.com>
2024-06-02 12:09:57 +0300

    Add Vue Files

---
+++ Learn_VueJS/src/main.js
@@ -0,0 +1,15 @@
+import "./assets/main.css";
+
+import { createApp } from "vue";
+import App from './App.vue'
+
+createApp(App).mount('#app')
+// const app = createApp({
+//   data() {
+//     return {
+//       message: "Hello Vue 3!",
+//     };
+//   },
+// });
+
+App.mount("#app");
